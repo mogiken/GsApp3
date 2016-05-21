d3b120514831bb1e685fb63878bb6cc536d00ba9
mogiken <mogi@hugg.co.jp>
2016-05-21 15:13:29 +0900

    ListView2

--- GsApp3/app/src/main/java/com/gashfara/mogi/gsapp3/MessageRecord.java
+++ GsApp3/app/src/main/java/com/gashfara/mogi/gsapp3/MessageRecord.java
@@ -3,19 +3,18 @@ package com.gashfara.mogi.gsapp3;
 
 public class MessageRecord {
     //保存するデータ全てを変数で定義します。
+    private String id;
     private String imageUrl;
     private String comment;
 
     //データを１つ作成する関数です。項目が増えたら増やしましょう。
-    public MessageRecord(String imageUrl, String comment) {
+    public MessageRecord(String id,String imageUrl, String comment) {
+        this.id = id;
         this.imageUrl = imageUrl;
         this.comment = comment;
     }
     //それぞれの項目を返す関数です。項目が増えたら増やしましょう。
-    public String getComment() {
-        return comment;
-    }
-    public String getImageUrl() {
-        return imageUrl;
-    }
-}
+    public String getComment(){return comment;}
+    public String getImageUrl() {return imageUrl;}
+    public String getId() {return id;}
+};
